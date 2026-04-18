578c7bd4b28f5cda06e228ffaa5fdd57f3f60e66
MateuszCuryllo <mateusz.curylo@student.wsb.edu.pl>
2026-04-18 12:02:37 +0200

    add

---
+++ C# celcius to farenheit/C# celcius to farenheit/celcius to farenheit.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Welcom in converter C# Celcius to Farenheit!");
+
+Console.WriteLine("choose what you wont to do\n \nPres 1 Conwert Celcius to Farenheit\nPres 2 Convert Farenheit to Celcius  ");
+int choos = int.Parse(Console.ReadLine());
+
+
+
+if  (choos == 1)
+{
+    Console.WriteLine($"Conwert Celcius to Farenheit enter value:  ");
+    float celcius =float.Parse(Console.ReadLine());
+    float result =celcius * 1.8f + 32 ;
+    Console.WriteLine($"{celcius}°C is {result}F");
+}
+if  (choos == 2)
+{
+    Console.WriteLine($"Convert Farenheit to Celcius enter value:  ");
+    float farenheit= float.Parse(Console.ReadLine());
+    float resut = (farenheit - 32) / 1.8f;
+    Console.WriteLine($"{farenheit}F is {resut}°C ");
+}
+else 
+{
+    Console.WriteLine("sometchink wet wrong");
+}
