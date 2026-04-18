8fa98352d98c6816e22bf5e5473f9fc183784013
MateuszCuryllo <mateusz.curylo@student.wsb.edu.pl>
2026-04-18 13:25:52 +0200

    Update C#_celcius_to_farenheit.cs

--- C#_celcius_to_farenheit.cs
+++ C#_celcius_to_farenheit.cs
@@ -1,25 +1,32 @@
-﻿Console.WriteLine("Welcom in converter C# Celcius to Farenheit!");
+﻿using System;
 
-Console.WriteLine("choose what you want to do\n \nPres 1 Conwert Celcius to Farenheit\nPres 2 Convert Farenheit to Celcius  ");
-int choos = int.Parse(Console.ReadLine());
-
-
-
-if  (choose == 1)
+class Program
 {
-    Console.WriteLine($"Conwert Celcius to Farenheit enter value:  ");
-    float celcius =float.Parse(Console.ReadLine());
-    float result =celcius * 1.8f + 32 ;
-    Console.WriteLine($"{celcius}°C is {result}F");
-}
-if  (choose == 2)
-{
-    Console.WriteLine($"Convert Farenheit to Celcius enter value:  ");
-    float farenheit= float.Parse(Console.ReadLine());
-    float resut = (farenheit - 32) / 1.8f;
-    Console.WriteLine($"{farenheit}F is {resut}°C ");
-}
-else 
-{
-    Console.WriteLine("sometching went wrong");
+    static void Main()
+    {
+        Console.WriteLine("Welcome to converter C# Celsius to Fahrenheit!");
+
+        Console.WriteLine("Choose what you want to do\n\nPress 1: Convert Celsius to Fahrenheit\nPress 2: Convert Fahrenheit to Celsius");
+        
+        int choose = int.Parse(Console.ReadLine());
+
+        if (choose == 1)
+        {
+            Console.WriteLine("Convert Celsius to Fahrenheit, enter value: ");
+            float celsius = float.Parse(Console.ReadLine());
+            float result = celsius * 1.8f + 32;
+            Console.WriteLine($"{celsius}°C is {result}F");
+        }
+        else if (choose == 2)
+        {
+            Console.WriteLine("Convert Fahrenheit to Celsius, enter value: ");
+            float fahrenheit = float.Parse(Console.ReadLine());
+            float result = (fahrenheit - 32) / 1.8f;
+            Console.WriteLine($"{fahrenheit}F is {result}°C");
+        }
+        else 
+        {
+            Console.WriteLine("Something went wrong - choose 1 or 2");
+        }
+    }
 }
