62ae9d0695d40b68b049b606a3e84bfda5cfda53
MateuszCuryllo <mateusz.curylo@student.wsb.edu.pl>
2026-04-18 13:14:05 +0200

    add

---
+++ C#_calculator.cs
@@ -0,0 +1,47 @@
+﻿Console.Write("Welcome in Calculator\n \nPlis give me first number: "); 
+float number_1 = float.Parse(Console.ReadLine());
+
+ Console.WriteLine("Chose what you want to do:\n - subtraction\n + addition\n * multiplication\n / division\n");
+string sign = Console.ReadLine();
+
+Console.WriteLine("Plis give me a second number");
+float number_2 = float.Parse(Console.ReadLine());
+float result = 0;
+bool succes = true;
+
+if (sign == "-" || sign == "subtraction")
+{
+    result = number_1 - number_2;
+}
+else if (sign == "+" || sign == "addition")
+{
+    result = number_1 + number_2;
+}
+else if (sign == "*" || sign == "multiplication")
+{
+    result = number_1 * number_2;
+}
+else if (sign == "/" || sign == "division")
+{
+    if (number_2 != 0)
+    {
+        result = number_1 / number_2;
+    }
+    else
+    {
+        number_2 = 0;
+        {
+            Console.WriteLine("do not divine by 0");
+            succes = false;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("sometching went wrong :{ try again ");
+    succes= false;
+}
+if (succes)
+{
+    Console.WriteLine($"result: {result}");
+}
---
+++ C#_celcius_to_farenheit.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Welcom in converter C# Celcius to Farenheit!");
+
+Console.WriteLine("choose what you want to do\n \nPres 1 Conwert Celcius to Farenheit\nPres 2 Convert Farenheit to Celcius  ");
+int choos = int.Parse(Console.ReadLine());
+
+
+
+if  (choose == 1)
+{
+    Console.WriteLine($"Conwert Celcius to Farenheit enter value:  ");
+    float celcius =float.Parse(Console.ReadLine());
+    float result =celcius * 1.8f + 32 ;
+    Console.WriteLine($"{celcius}°C is {result}F");
+}
+if  (choose == 2)
+{
+    Console.WriteLine($"Convert Farenheit to Celcius enter value:  ");
+    float farenheit= float.Parse(Console.ReadLine());
+    float resut = (farenheit - 32) / 1.8f;
+    Console.WriteLine($"{farenheit}F is {resut}°C ");
+}
+else 
+{
+    Console.WriteLine("sometching went wrong");
+}
