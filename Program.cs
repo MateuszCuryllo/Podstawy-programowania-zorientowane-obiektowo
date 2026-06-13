f04b412a60a12fdeede986e774e0422dbebba8e3
MateuszCuryllo <mateusz.curylo@student.wsb.edu.pl>
2026-06-13 16:04:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace WypozyczalniaGier
+{
+  
+    public class GraKomputerowa
+    {
+       
+        public string Tytul { get; set; }
+        public string Platforma { get; set; }
+        public bool CzyWypozyczona { get; private set; }
+        public decimal KosztWypozyczenia { get; } 
+     
+        public void Wypozycz()
+        {
+          CzyWypozyczona =true;
+        }
+        public void Zwroc()
+        {
+           CzyWypozyczona =false;
+        }
+        public void PokazInformacje()
+        {
+            
+        }
+    }
+
+    public class ZarzadcaWypozyczalni
+    {
+        public List<GraKomputerowa> KatalogGier = new List<GraKomputerowa>();
+
+        public void WyswietlWszystkieGry()
+        {
+
+        }
+    }
+}
